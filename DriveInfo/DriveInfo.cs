c951ef8368f663d4eb129b17621419e4b200c57f
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-08 14:43:17 +0300

    Добавьте файлы проекта.

---
+++ DriveInfo/DriveInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveInfo
+{
+    internal class DriveInfo
+    {
+        public DriveInfo(string name, double size, double freeSpace)
+        {
+            Name = name;
+            TotalSpace = size;
+            FreeSpace = freeSpace;
+        }
+        public string Name
+        {
+            get;
+        }
+         public double TotalSpace
+        {
+            get; 
+        }
+        public double FreeSpace
+        {
+            get; 
+        }
+    }
+}
---
+++ DriveInfo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DriveInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo drive = new DriveInfo("C", 521, 124);
+            Console.WriteLine(drive.TotalSpace);
+        }
+    }
+}
