742f05c4575995255154c54eecae40834470fe26
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-10 13:53:12 +0300

    8.4.2 - Сохранение даты работы с файлом

--- BinaryReader/Program.cs
+++ BinaryReader/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Reader
+﻿using System.Globalization;
+
+namespace Reader
 {
     internal class Program
     {
@@ -11,9 +13,16 @@
             string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\" + file;
             if (File.Exists(path)) 
             {
+                DateTime date;
                 using (BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open)))
                 {
+                    date = DateTime.Now;
                     Console.WriteLine(br.ReadString());    
+                }
+
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Append)))
+                {
+                    writer.Write($"\nФайл изменен {date.ToString("M/MM HH:mm", CultureInfo.CurrentCulture)} на компьютере {Environment.OSVersion}");
                 }
             }
         } 
