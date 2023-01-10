108cc60a78ca2fe088a1f0282179cf5a1a379d1e
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-10 13:17:47 +0300

    8.4.1 - Чтение бинарного файла

---
+++ BinaryReader/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Reader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDataFromBinaryFile("BinaryFile.bin");
+        }
+        private static void PrintDataFromBinaryFile(string file)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\" + file;
+            if (File.Exists(path)) 
+            {
+                using (BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open)))
+                {
+                    Console.WriteLine(br.ReadString());    
+                }
+            }
+        } 
+    }
+}
