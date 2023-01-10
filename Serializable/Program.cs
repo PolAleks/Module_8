aadfcc47aaab0e7ab7c585d799d5be1424f43aad
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-10 15:31:52 +0300

    8.4.3 - Сериализация объекта в бинарный файл

---
+++ Serializable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serializable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact("Вася Пупкин", 89781234567, "vs.pupkin@ya.ru");
+            Saveobject(contact);
+        }
+        static void Saveobject(Contact contact)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + '/';
+
+            using (FileStream fstream = new FileStream($"{path}MyConact.bin", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fstream, contact);
+            }
+        }
+    }
+}
